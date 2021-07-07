8402ad8752a6a310bd5ebd685476fa901916757c
Pedro Fernandes <pedro.mdr.fernandes@gmail.com>
2021-07-07 23:01:38 +0100

    Updating the parameters and merging

--- run_full.py
+++ run_full.py
@@ -8,42 +8,42 @@ gen_param_specs = {
     'initial_rock_density' : {
         'dtype' : float,
         'min' : 0.0,
-        'max' : 0.5
+        'max' : 0.3
     },
     'initial_tree_density' : {
         'dtype' : float,
         'min' : 0.0,
-        'max' : 0.5
+        'max' : 0.3
     },
     'rock_refinement_runs' : {
         'dtype' : int,
         'min' : 0,
-        'max' : 5
+        'max' : 2
     },
     'tree_refinement_runs' : {
         'dtype' : int,
         'min' : 0,
-        'max' : 5
+        'max' : 2
     },
     'rock_neighbour_depth' : {
         'dtype' : int,
         'min' : 1,
-        'max' : 1
+        'max' : 2
     },
     'tree_neighbour_depth' : {
         'dtype' : int,
         'min' : 1,
-        'max' : 1
+        'max' : 2
     },
     'rock_neighbour_number' : {
         'dtype' : int,
         'min' : 3,
-        'max' : 3
+        'max' : 8
     },
     'tree_neighbour_number' : {
         'dtype' : int,
         'min' : 3,
-        'max' : 3
+        'max' : 8
     },
     'base_clear_depth' : {
         'dtype' : int,
