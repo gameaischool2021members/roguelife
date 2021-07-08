8dea5b2ebbc2d2df3661695cfda368d72803b135
Niels NTG <NTG999@gmail.com>
2021-07-08 14:30:41 +0200

    Fix random runner

--- run_random.py
+++ run_random.py
@@ -1,13 +1,13 @@
-
+from evo.evo import EvoAlg
 from game.game import Game
-import random 
+import random
 import time
+from run_full import gen_param_specs
 
-env = Game()
-
-
+dummyEvoSystem = EvoAlg(spec=gen_param_specs)
+env = Game(evo_system=dummyEvoSystem)
 
 while True:
     pil_image, reward, done, thing = env.step(env.action_space.sample())
     if done:
-    	env.reset()
+        env.reset()
